c30a29940268855e3ef406137655865d2eff09c3
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2025-05-03 00:52:50 -0300

    paginaConGsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static', // Esto asegura que se generen archivos estáticos
+  integrations: [
+    tailwind({
+      // Asegurarse de que Tailwind use el archivo de configuración correcto
+      config: { path: './tailwind.config.ts' },
+    })
+  ]
+});
