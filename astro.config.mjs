ee645edd6207b91a7f23ec46ec19e4c66020f642
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2025-05-09 09:43:07 -0300

    UpdateContactAndConfiguraconMotoresDebusquedaGoogle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zenithstudio.site', // Importante: añade tu URL
   devToolbar: {
     enabled: false, // Desactiva la toolbar correctamente
   },
@@ -10,6 +13,8 @@ export default defineConfig({
   integrations: [
     tailwind({
       config: { path: './tailwind.config.ts' },
-    })
+    }),
+    sitemap(), // Genera automáticamente el sitemap.xml
+    robotsTxt() // Genera automáticamente el robots.txt
   ]
 });
